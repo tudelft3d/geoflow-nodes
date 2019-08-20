235706bd52fdc49c3337d25491714c0684338daf
Ylannl <r.y.peters@tudelft.nl>
2019-08-20 15:22:39 +0200

    remove headers that are deprecated in CGAL 5.0

--- modules/stepedge/point_edge.h
+++ modules/stepedge/point_edge.h
@@ -18,8 +18,6 @@
 #include <CGAL/pca_estimate_normals.h>
 #include <CGAL/mst_orient_normals.h>
 #include <CGAL/property_map.h>
-#include <CGAL/Shape_detection_3.h>
-#include <CGAL/regularize_planes.h>
 #include <CGAL/Simple_cartesian.h>
 #include <CGAL/point_generators_3.h>
 #include <CGAL/Orthogonal_k_neighbor_search.h>
@@ -65,14 +63,6 @@ typedef CGAL::Parallel_tag Concurrency_tag;
 #else
 typedef CGAL::Sequential_tag Concurrency_tag;
 #endif
-// In Shape_detection_traits the basic types, i.e., Point and Vector types
-// as well as iterator type and property maps, are defined.
-typedef CGAL::Shape_detection_3::Shape_detection_traits
-  <Kernel, PNL_vector, Point_map, Normal_map>      Traits;
-typedef CGAL::Shape_detection_3::Efficient_RANSAC<Traits>    Efficient_ransac;
-typedef CGAL::Shape_detection_3::Region_growing<Traits>      Region_growing;
-typedef CGAL::Shape_detection_3::Plane<Traits>               SCPlane;
-
 // search tree
 // typedef boost::tuple<Point_3,int>                           Point_and_int;
 // typedef CGAL::Random_points_in_cube_3<Point_3>              Random_points_iterator;
@@ -265,37 +255,6 @@ class Snap_observer : public CGAL::Arr_observer<Arrangement_2>
     if (old_face->data().segid==0)
       new_face->set_data(old_face->data());
   }
-};
-
-// this is for the cgal regularize_plane function
-class Index_map
-{
-  boost::shared_ptr<std::vector<int> > m_indices;
-  // const std::vector<int> m_indices;
-public:
-  typedef int value_type; ///< Index of the shape (-1 if the point is not assigned to any shape).
-  typedef int reference;
-  typedef boost::readable_property_map_tag category;
-
-  Index_map(){}
-
-  /*!
-    Constructs a property map to map points to their associated shape.
-    \note `shapes` must be a range of shapes detected using `points`.
-    \tparam ShapeRange an `Iterator_range` with a bidirectional
-    constant iterator type with value type
-    `boost::shared_ptr<CGAL::Shape_detection_3::Shape_base<Traits> >`.
-    */
-  Index_map (const std::vector<int> intvector)
-    : m_indices (new std::vector<int>(intvector))
-  {
-  }
-
-  inline friend int get (const Index_map& pm, const size_t& k)
-  {
-    return (*(pm.m_indices))[k];
-  }
-
 };
 
 namespace bg = boost::geometry;
